a8acfcdd156340d2b5645d20ca2077dd1b6f71e8
imshubhampatel <shubhampatel2024@gmail.com>
2023-08-26 03:47:20 +0530

    Feat : Added Eureka Server and Eureka Client communication

--- students-connect-service/src/main/java/com/bt/management/microservices/studentsconnectservice/StudentsConnectServiceApplication.java
+++ students-connect-service/src/main/java/com/bt/management/microservices/studentsconnectservice/StudentsConnectServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudentsConnectServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentsConnectServiceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(StudentsConnectServiceApplication.class, args);
+  }
 }
